db8c053ce65fbd42881fc10f39daa04ca1f71242
Thitiwudh Uasmith <thitiwudh.u@mail.kmutt.ac.th>
2022-09-03 00:11:49 +0700

    feat: Added solutions for the 3rd question with 2 and 3 stars.

---
+++ no3_star2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        double result = Math.Pow(2, n);
+        Console.WriteLine("{0:N0}",result);
+    }
+}
