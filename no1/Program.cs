492c1c9fcdcaa04a1787eb7b941a420235015b11
Thitiwudh Uasmith <thitiwudh.u@mail.kmutt.ac.th>
2022-09-02 23:51:37 +0700

    feat: Added solution for the 1st question.

---
+++ no1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sum;
+        Console.Write("Please input number 1: ");
+        sum = int.Parse(Console.ReadLine());
+        Console.Write("Please input number 2: ");
+        sum += int.Parse(Console.ReadLine());
+        Console.Write("Please input number 3: ");
+        sum += int.Parse(Console.ReadLine());
+        Console.WriteLine("Sum: {0}", sum);
+    }
+}
