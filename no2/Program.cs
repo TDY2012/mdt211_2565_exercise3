69ab53a612b28c0e4a413f5cecfaa9a19dc135e4
Thitiwudh Uasmith <thitiwudh.u@mail.kmutt.ac.th>
2022-09-02 23:55:51 +0700

    feat: Added a solution for the 2nd question.

---
+++ no2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float a, b, x;
+        a = float.Parse(Console.ReadLine());
+        b = float.Parse(Console.ReadLine());
+        x = float.Parse(Console.ReadLine());
+        Console.WriteLine(a <= x && x <= b);
+    }
+}
