7b8960ef9b14510129d949e01e9f0f844cedcdb5
Thitiwudh Uasmith <thitiwudh.u@mail.kmutt.ac.th>
2022-09-03 00:00:34 +0700

    feat: Added a solution for the 3rd question with 1 star.

---
+++ no3_star1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int result = 1;
+
+        while(n > 0)
+        {
+            result *= 2;
+            n--;
+        }
+
+        Console.WriteLine(result);
+    }
+}
