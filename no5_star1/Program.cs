1c1a064c46e644fad5047949b33922d3a6427dd6
Thitiwudh Uasmith <thitiwudh.u@mail.kmutt.ac.th>
2022-09-03 00:47:36 +0700

    feat: Added solutions for the 5th question.

---
+++ no5_star1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int seatNo = int.Parse(Console.ReadLine());
+
+        if(seatNo + 1 > 8)
+        {
+            Console.WriteLine(seatNo + 1 - 8);
+        }
+        else
+        {
+            Console.WriteLine(seatNo + 1);
+        }
+
+        if(seatNo + 2 > 8)
+        {
+            Console.WriteLine(seatNo + 2 - 8);
+        }
+        else
+        {
+            Console.WriteLine(seatNo + 2);
+        }
+
+        if(seatNo + 3 > 8)
+        {
+            Console.WriteLine(seatNo + 3 - 8);
+        }
+        else
+        {
+            Console.WriteLine(seatNo + 3);
+        }
+
+        if(seatNo + 4 > 8)
+        {
+            Console.WriteLine(seatNo + 4 - 8);
+        }
+        else
+        {
+            Console.WriteLine(seatNo + 4);
+        }
+    }
+}
---
+++ no5_star2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int seatNo = int.Parse(Console.ReadLine());
+
+        Console.WriteLine((seatNo % 8) + 1);
+        Console.WriteLine(((seatNo + 1) % 8) + 1);
+        Console.WriteLine(((seatNo + 2) % 8) + 1);
+        Console.WriteLine(((seatNo + 3) % 8) + 1);
+    }
+}
